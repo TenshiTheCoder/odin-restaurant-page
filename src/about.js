bdc0ee76bf7be74679220a23d4f3aff05f5391c0
TenshiTheCoder <tenshibusinessbrand@gmail.com>
2025-07-29 12:44:54 -0400

    Complete: initial draft of home page, Move: several elements that fit better to the about page

--- src/about.js
+++ src/about.js
@@ -14,6 +14,50 @@ export function createAbout() {
 
     contentContainer.appendChild(aboutHeading);
     contentContainer.appendChild(aboutDescription);
+
+     // Container for the email container info
+  const emailContainer = document.createElement("div");
+  emailContainer.classList.add("email-container");
+
+  const emailLabel = document.createElement("strong");
+  emailLabel.textContent = "Email: ";
+  emailLabel.classList.add("email-label");
+
+  const emailAddress = document.createElement("p");
+  emailAddress.textContent = "bellCafe540@business.com";
+  emailAddress.classList.add("email-info");
+
+  emailContainer.append(emailLabel, emailAddress);
+
+  // Address Info
+  const addressContainer = document.createElement("div");
+  addressContainer.classList.add("address-container");
+
+  const addressLabel = document.createElement("strong");
+  addressLabel.textContent = "Visit us in store at: ";
+  addressLabel.classList.add("address-label");
+
+  const addressInfo = document.createElement("p");
+  addressInfo.textContent = "540 Bell Ave, New York City";
+  addressInfo.classList.add("address-info");
+
+  addressContainer.append(addressLabel, addressInfo);
+
+  // Container for the phone info 
+  const phoneContainer = document.createElement("div");
+  phoneContainer.classList.add("phone-container");
+
+  const phoneLabel = document.createElement("strong");
+  phoneLabel.textContent = "Phone Number: ";
+  phoneLabel.classList.add("phone-label");
+
+  const phoneNumber = document.createElement("p");
+  phoneNumber.textContent = "123-1234-4512";
+  phoneNumber.classList.add("phone-info");
+
+  phoneContainer.append(phoneLabel, phoneNumber);
+
+  // contactContainer.append(phoneContainer, emailContainer, addressContainer);
 };
 
 
--- src/home.js
+++ src/home.js
@@ -18,9 +18,10 @@ export function createHome() {
 
   // Container for the testimonial(s)
   const testContainer = document.createElement("div");
+  testContainer.classList.add("test-container");
 
   const testimonial = document.createElement("p");
-  testimonial.classList.add("testimonial");
+  testimonial.classList.add("testimonial-text");
   testimonial.textContent = "Bell Cafe is a hidden gem in New York! The ambiance is heavenly, and the pasta truly lives up to its divine reputation. Every visit feels like a special occasion.";
 
   const testAttr = document.createElement("p");
@@ -31,6 +32,7 @@ export function createHome() {
 
   // Container for the featured dish
   const featuredContainer = document.createElement("div");
+  featuredContainer.classList.add("featured-container");
 
   const dishImg = document.createElement("img");
   dishImg.classList.add("fettuccine-img");
@@ -40,7 +42,8 @@ export function createHome() {
   dishDescription.classList.add("featured-description");
   dishDescription.textContent = "Creamy, rich Fettuccine Alfredo made with fresh parmesan and buttery sauce — a timeless classic that comforts and delights every bite."
   
-  featuredContainer.append(dishImg, dishDescription);
+  featuredContainer.append(dishImg, dishDescription,);
+
   // Container for the phone contact info
   const contactContainer = document.createElement("div");
   contactContainer.classList.add("contact-container");
@@ -48,50 +51,6 @@ export function createHome() {
   const contactHeading = document.createElement("h1");
   contactHeading.textContent = "Contact Us";
   contactHeading.classList.add("contact-heading");
-
-  const phoneContainer = document.createElement("div");
-  phoneContainer.classList.add("phone-container");
-
-  const phoneLabel = document.createElement("strong");
-  phoneLabel.textContent = "Phone Number: ";
-  phoneLabel.classList.add("phone-label");
-
-  const phoneNumber = document.createElement("p");
-  phoneNumber.textContent = "123-1234-4512";
-  phoneNumber.classList.add("phone-info");
-
-  phoneContainer.append(phoneLabel, phoneNumber);
-
-  // Container for the email container info
-  const emailContainer = document.createElement("div");
-  emailContainer.classList.add("email-container");
-
-  const emailLabel = document.createElement("strong");
-  emailLabel.textContent = "Email: ";
-  emailLabel.classList.add("email-label");
-
-  const emailAddress = document.createElement("p");
-  emailAddress.textContent = "bellCafe540@business.com";
-  emailAddress.classList.add("email-info");
-
-  emailContainer.append(emailLabel, emailAddress);
-
-  // Address Info
-  const addressContainer = document.createElement("div");
-  addressContainer.classList.add("address-container");
-
-  const addressLabel = document.createElement("strong");
-  addressLabel.textContent = "Visit us in store at: ";
-  addressLabel.classList.add("address-label");
-
-  const addressInfo = document.createElement("p");
-  addressInfo.textContent = "540 Bell Ave, New York City";
-  addressInfo.classList.add("address-info");
-
-  addressContainer.append(addressLabel, addressInfo);
-
-  // Appending the different sections to the parent contact container
-  contactContainer.append(phoneContainer, emailContainer, addressContainer);
 
   // Store hours info
   const hoursContainer = document.createElement("section");
@@ -135,6 +94,24 @@ export function createHome() {
   hoursArticle.append(hoursLabel, monday, tuesday, wednesday, thursday, friday, saturday, sunday);
   hoursContainer.appendChild(hoursArticle);
 
+  // Why Us Section
+  const whyContainer = document.createElement("div");
+  whyContainer.classList.add("why-container");
+
+  const whyHeading = document.createElement("h3");
+  whyHeading.classList.add("why-heading"); 
+  whyHeading.textContent = "Why Us?";
+  
+  const whyText = document.createElement("p");
+  whyText.classList.add("why-text");
+  whyText.textContent = "At Bell Cafe, we believe every meal should feel like a moment of serenity. Nestled in the heart of New York, we craft each dish with the finest ingredients and a deep love for tradition. Whether you're savoring our heavenly pasta or sipping a warm cappuccino under the soft morning light, our welcoming atmosphere and attentive service make every visit feel special. Come for the food — stay for the feeling."
+  
+  whyContainer.append(whyHeading, whyText);
+  // Container to properly position the hours, featured dish and Why Us Container
+  const midSection = document.createElement("div");
+  midSection.classList.add("mid-section");
+  midSection.append(whyContainer, featuredContainer, hoursContainer);
+
   // Appending the finished containers to the #content parent div
-  contentContainer.append(homeHeadingContainer, testContainer, featuredContainer, contactContainer, hoursContainer);
+  contentContainer.append(homeHeadingContainer, testContainer, midSection);
 }; 
